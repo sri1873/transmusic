41fa7f736a56d2afd5ccefda4c7b05a8cfc56203
Sri Kumar <kssrikumar180703@gmail.com>
2023-08-07 08:26:18 +0530

    Added svg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Home from './Components/Home';
 import React from "react";
 import Navbar from "./Components/Navbar";
+import About from './Components/About';
 function App() {
   return (
     <div className="App bg-neutral-100">
       <Navbar />
       <Home />
-
+      <About/>
     </div>
   );
 }
